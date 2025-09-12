5aa2a41f41c00f53db24218c4427d77c1ec2c9e1
manish <manishkumar032004@gmail.com>
2025-09-12 09:14:53 +0530

    subbarrays

---
+++ subbarray.java
@@ -0,0 +1,23 @@
+public class subbarray {
+    public static void subbarray1(int number[]) {
+        int tp=0;
+        for(int i=0;i<number.length;i++){
+            for(int j =i;j<number.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(number[k]+" "); 
+                }
+                tp++;
+                System.out.println();
+            }
+            System.out.println();
+           
+        }
+        System.out.println("total subbarray ="+tp);
+        
+    }
+    public static void main(String[] args) {
+        int number[]={1,2,3,4,5};
+        subbarray1(number);
+    }
+    
+}
