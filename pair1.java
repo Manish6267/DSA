86ca5fd95d7988d0db2396668af3a8ab53bf71b1
manish <manishkumar032004@gmail.com>
2025-09-15 23:40:16 +0530

    upadted

--- pair1.java
+++ pair1.java
@@ -1,19 +1,20 @@
 public class pair1 {
-    public static void findPairs(int arr[], int target) {
-        System.out.println("Pairs with sum = " + target + " are:");
+    public static void findPairs(int arr[]) {
+       
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) {
-                    System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+                
+                    System.out.print("(" + arr[i] + ", " + arr[j] + ")");
                 }
+                System.out.println();
             }
         }
-    }
+    
 
     public static void main(String[] args) {
         int arr[] = {2, 4, 3, 7, 1, 6,5};
-        int target = 6;
-        findPairs(arr, target);
+        
+        findPairs(arr);
     }
 }
 
