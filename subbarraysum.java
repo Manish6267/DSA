7278f6ad705706c788590c6e9abe9827bfe9abee
manish <manishkumar032004@gmail.com>
2025-09-17 23:13:15 +0530

    update

--- subbarraysum.java
+++ subbarraysum.java
@@ -21,7 +21,7 @@ public class subbarraysum {
 
     
     public static void main(String[] args) {
-        int number[]={1,2,3};
+        int number[]={1,2,3,4};
         subbarraysum1(number);
 
     }
