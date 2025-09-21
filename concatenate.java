baf3a713abb93423c9137cb36f00e3819c3b9546
manish <manishkumar032004@gmail.com>
2025-09-21 15:49:42 +0530

    add two name in string

---
+++ concatenate.java
@@ -0,0 +1,9 @@
+public class concatenate {
+    public static void main(String[] args) {
+        String firstname="manish";
+        String lastname="kumar";
+        String fullname=firstname+"  "+lastname;
+        System.out.println(fullname);
+    }
+    
+}
