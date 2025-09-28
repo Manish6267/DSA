3c212ec5767e3daa5d856cb4113454fe4e333a13
manish <manishkumar032004@gmail.com>
2025-09-28 22:48:08 +0530

    update

--- length.java
+++ length.java
@@ -1,6 +1,6 @@
 public class length {
     public static void main(String[] args) {
-        String fullname="manish kumar";
+        String fullname="ayush kumar";
         System.out.println(fullname.length());
     }
     
