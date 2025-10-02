dc9656a976f3d7ee095232d3b929bfae618d3a30
manish <manishkumar032004@gmail.com>
2025-10-02 23:36:31 +0530

    update

--- subbarray.java
+++ subbarray.java
@@ -16,7 +16,7 @@ public class subbarray {
         
     }
     public static void main(String[] args) {
-        int number[]={1,2,3,4,5,6,7};
+        int number[]={1,2,3,4,5,6,7,8};
         subbarray1(number);
     }
     
