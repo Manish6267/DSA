255f674a841429b088435a0f80dd11ce970f4a53
manish <manishkumar032004@gmail.com>
2025-10-23 19:07:59 +0530

    update

--- concatenate.java
+++ concatenate.java
@@ -1,6 +1,6 @@
 public class concatenate {
     public static void main(String[] args) {
-        String firstname="anuragh";
+        String firstname="nikhil";
         String lastname="kumar";
         String fullname=firstname+"  "+lastname;
         
