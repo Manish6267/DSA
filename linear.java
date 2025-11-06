9c8da99f3039f5c370e6b290944b5b7760805650
manish <manishkumar032004@gmail.com>
2025-11-06 21:48:01 +0530

    dsa

--- linear.java
+++ linear.java
@@ -8,6 +8,8 @@ public class linear {
         return -1;
         
     }
+
+    
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,9,10,20,30,40,50};
         int key=55;
