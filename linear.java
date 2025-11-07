eb9900bced51b3ef1df21871860ecbd24ec4b2f5
manish <manishkumar032004@gmail.com>
2025-11-07 23:34:51 +0530

    DSA

--- linear.java
+++ linear.java
@@ -13,9 +13,6 @@ public class linear {
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,9,10,20,30,40,50};
         int key=55;
-       
-        
-        
             System.out.println("key found __"+linearsearch(numbers, key));
        
     }
